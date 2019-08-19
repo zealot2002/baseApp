94c2803f9d846149dd859e24d046c832d1213651
zealot2002 <zzy_2002@126.com>
2019-08-19 22:30:44 +0800

    页面都加上了

---
+++ user/src/main/java/com/zzy/login/presenter/MainPresenter.java
@@ -0,0 +1,89 @@
+package com.zzy.login.presenter;
+import android.support.annotation.NonNull;
+
+import com.zzy.commonlib.http.HConstant;
+import com.zzy.commonlib.http.HInterface;
+import com.zzy.commonlib.utils.AppUtils;
+import com.zzy.commonlib.utils.NetUtils;
+import com.zzy.login.contract.MineContract;
+import com.zzy.login.model.HttpProxy;
+import com.zzy.login.model.bean.main.BannerBean;
+import com.zzy.login.model.wrapper.HfCtx;
+
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author dell-7020
+ * @Description:
+ * @date 2018/08/07 16:25:23
+ */
+
+public class MainPresenter implements MineContract.Presenter{
+    private final MineContract.View view;
+    private HfCtx hfCtx;
+    private AtomicInteger dog;
+/****************************************************************************************************/
+    public MainPresenter(@NonNull MineContract.View view) {
+        this.view = view;
+        hfCtx = new HfCtx();
+        dog = new AtomicInteger(0);
+    }
+    @Override
+    public void start() {
+        if (!NetUtils.isNetworkAvailable(AppUtils.getApp())) {
+            view.showDisconnect();
+            return;
+        }
+        view.showLoading();
+        dog.set(0);
+        try{
+            getBannerList();
+        }catch(Exception e){
+            e.printStackTrace();
+            handleErrs(e.toString());
+        }
+    }
+
+
+    private void getBannerList() throws Exception{
+        HttpProxy.getBannerList(new HInterface.DataCallback() {
+            @Override
+            public void requestCallback(int result, Object o, Object o1) {
+                if (result == HConstant.SUCCESS) {
+                    hfCtx.setBannerList((List<BannerBean>) o);
+                    updateUI();
+                }else if(result == HConstant.INTERCEPTED) {
+                    //do nothing
+                }else{
+                    handleErrs((String) o);
+                }
+            }
+        });
+    }
+
+    private void updateUI(){
+        /**
+        getBannerList();
+        getNoticeList();
+        getProjectInfo();
+        getUpperShortcutList();
+        getUnderShortcutList();
+        都回来之后，刷新ui
+        **/
+        if(dog.incrementAndGet() == 1) {
+            view.closeLoading();
+            view.updateUI(hfCtx);
+        }
+    }
+    private void handleErrs(String s){
+        view.closeLoading();
+        view.showDisconnect();
+      //  ToastUtils.showShort(s);
+    }
+
+    @Override
+    public void getUserInfo() {
+
+    }
+}
---
+++ user/src/main/java/com/zzy/login/view/activity/MyEntrepreneurshipActivity.java
@@ -0,0 +1,56 @@
+package com.zzy.login.view.activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.zzy.common.base.BaseToolbarActivity;
+import com.zzy.user.R;
+
+/**
+ * 我的创业
+ */
+public class MyEntrepreneurshipActivity extends BaseToolbarActivity implements View.OnClickListener {
+    private EditText etPhone,etPassword;
+    private Button btnOk;
+    private TextView tvToBePioneer,tvForgetPassword;
+
+/***********************************************************************************************/
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_school_fragment);
+        setupViews();
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    private void setupViews() {
+//        etPhone = findViewById(R.id.etPhone);
+//        etPassword = findViewById(R.id.etPassword);
+//        btnOk = findViewById(R.id.btnOk);
+//        tvToBePioneer = findViewById(R.id.tvToBePioneer);
+//        tvForgetPassword = findViewById(R.id.tvForgetPassword);
+
+        btnOk.setOnClickListener(this);
+        tvToBePioneer.setOnClickListener(this);
+        tvForgetPassword.setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+//        if(v.getId() == R.id.btnOk){
+//            // TODO: 2019/8/19   to login
+//        }else if(v.getId() == R.id.tvToBePioneer){
+//
+//        }else if(v.getId() == R.id.tvForgetPassword){
+//
+//        }
+
+    }
+}
